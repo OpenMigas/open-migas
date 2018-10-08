00933eccec537060912b23864a713ece3e0e2f33
Julian Soto <juliansotoponce@gmail.com>
2018-10-07 20:41:58 -0700

    Change folder tree handling, install electron-tree-view module

--- app/scripts/master.js
+++ app/scripts/master.js
@@ -1,18 +1,43 @@
 const remote = require('electron').remote;
-const dialog = remote.dialog;
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').execFile;
 const spawn = require('child_process').spawn;
-const {Menu, MenuItem} = remote;
+const {Menu, MenuItem, dialog} = remote;
 const trash = require('trash');
 const deleteConfirmationDialog = require('../view/delete-dialog.js');
+const vdom = require('virtual-dom');
+const hyperx = require('hyperx');
+const hx = hyperx(vdom.h);
 
 var selected = new Selected('file-selected');
 
 function init(){
 	var win = remote.getCurrentWindow();
-	let explorerPath = ['C:\\'];
+
+	function History(root){
+		var history = root ? [root] : [];
+		var position = 0;
+		this.add = function(path){
+			history.splice(position + 1);
+			history.push(path);
+			position = history.length - 1;
+		}
+		this.goBack = function(){
+			if (position > 0) position--;
+		}
+		this.goForward = function(){
+			if (position < history.length - 1) position++;
+		}
+		this.getCurrentLocation = function(){
+			return history[position];
+		}
+		this.getHistoryPath = function(){
+			return history;
+		}
+	}
+
+	let history = new History();
 
 	window.addEventListener('contextmenu', (e) => {
 		e.preventDefault();
@@ -34,14 +59,48 @@ function init(){
 		win.minimize();
 	});
 
-	updateLocation(explorerPath, null, document.querySelector('#system-drive'));
+	const root = {
+		name: 'C:',
+		fullname: 'C:\\',
+		children: []
+	};
+
+	const tree = require('electron-tree-view')({
+		root,
+		container: document.querySelector('#folder-tree'),
+		children: c => c.children,
+		label: c => c.name,
+		renderItem: (hx, data, children, loadHook, clickElem, createChild) => {
+			return hx`<div class="" loaded=${loadHook}>
+				<a href="#" class="clickable-elem" onclick=${clickElem}>
+				<div>
+					<span>${hx`<i class="icon-right-open chevron" />`} ${data.name}</span>
+				</div>
+				</a>
+				<ul>
+					${children.map(createChild)}
+				</ul>
+			</div>`
+		}
+	});
+
+	tree.on('selected', function(ele){
+		loadView(ele.fullname, (childElements) => {
+			ele.children = childElements;
+			history.add(ele.fullname);
+			console.log(history.getHistoryPath());
+			tree.loop.update({root});
+		});
+	});
 
 	document.querySelector('#up').addEventListener('click', function(){
-		updateLocation(explorerPath, '../');
+	//	updateLocation(explorerPath, '../');
 	});
 
 	document.querySelector('#reload').addEventListener('click', function(){
-		updateLocation(explorerPath);
+		loadView(history.getCurrentLocation(), function(childElements){
+			tree.loop.update({root});
+		});
 	});
 
 	win.on('resize', function(){
@@ -55,6 +114,230 @@ function init(){
 	});
 }
 
+function loadView(fullname, loaded){
+	readFolder(fullname, (childFiles) => {
+		let childElements = [];
+		childFiles.forEach((item, index) => {
+			if (item.stat.isDirectory()){
+				childElements[index] = {
+					name: item.name,
+					fullname: path.resolve(fullname, item.name),
+					children: []
+				};
+			}
+		});
+		updateLocation(fullname, childFiles, () => {
+			//go to another location
+		});
+		loaded(childElements);
+	});
+}
+
+function readFolder(location, done){
+	let filesArr = [];
+	fs.readdir(location, function(err, files){
+		if (err) return console.error(err);
+		files.forEach((item, index) => {
+			try {
+				let stat;
+				stat = fs.statSync(path.resolve(location, item));
+				filesArr[index] = {name: item, stat: stat};
+			} catch(err) {
+				console.error(err);
+				return;
+			}
+		});
+		done(filesArr);
+	});
+}
+
+//updates files main view and breadcrumb bar
+//callback called when user select another location
+function updateLocation(location, files, callback){
+	let filesContainer = document.querySelector('#explorer-container');
+	filesContainer.innerHTML = '';
+	files.forEach(function(file){
+		//explorer main view item
+		let fileItem = createExplorerItem(location, file.name, file.stat, callback);
+		filesContainer.appendChild(fileItem);
+	});
+	updateBreadcrumbLocation(location, callback);
+}
+
+//breadcrumb bar location
+function updateBreadcrumbLocation(location, callback){
+	let breadcrumbBar = document.querySelector('#breadcrumb-bar');
+	let splitLocation = location.split(path.sep);
+
+	//remove empty string
+	if (splitLocation[splitLocation.length - 1] === '') splitLocation.splice(-1, 1);
+	//clear bar
+	breadcrumbBar.innerHTML = '';
+
+	splitLocation.forEach(function(item, index){
+		let locationSubfolder = document.createElement('span');
+		locationSubfolder.setAttribute('class', 'location-element');
+
+		locationSubfolder.innerHTML = item;
+
+		//get current iteration location
+		let locationSliced = splitLocation.slice(0, index + 1);
+		let itemLocation = locationSliced.join(path.sep);
+
+		locationSubfolder.addEventListener('click', function(e){
+			callback(itemLocation);
+		});
+
+		breadcrumbBar.appendChild(locationSubfolder);
+
+		//add a separator
+		if (index < splitLocation.length - 1){
+			let itemSeparator = document.createElement('span');
+			itemSeparator.setAttribute('class', 'location-separator');
+			itemSeparator.innerHTML = '<i class="icon-right-open"></i>';
+			breadcrumbBar.appendChild(itemSeparator);
+		}
+	});
+}
+
+//main explorer view items
+function createExplorerItem(location, name, stat, goto){
+	var ele = document.createElement('span');
+	let fullname = path.resolve(location, name);
+	ele.innerHTML = name;
+
+	if (stat.isFile()){
+		let open = function(){
+			let child = exec('explorer', [fullname], (err, stdout, stderr) => {
+				if (err){
+					console.error(err);
+				}
+			});
+		}
+
+		ele.addEventListener('dblclick', function(e){
+			open();
+		});
+
+		ele.addEventListener('contextmenu', function(e){
+			let template = createFileContextMenu(fullname, open);
+			const menu = Menu.buildFromTemplate(template);
+			menu.popup({window: remote.getCurrentWindow()});
+		});
+		ele.innerHTML = '<i class="icon-doc element-item-icon file-icon"></i>' + name;
+	} else if (stat.isDirectory()){
+		ele.addEventListener('contextmenu', function(e){
+			let template = createFolderContextMenu(fullname, function(){console.log('open folder')});
+			const menu = Menu.buildFromTemplate(template);
+			menu.popup({window: remote.getCurrentWindow()});
+		});
+		ele.innerHTML = '<i class="icon-folder-open element-item-icon folder-icon"></i>' + name;
+	}
+
+	ele.addEventListener('dblclick', function(e){
+		if (stat.isFile()){
+			//open with default program
+		} else if (stat.isDirectory()){
+			//open this folder
+			//goto location
+		}
+	});
+
+	function openFile(){
+
+	}
+
+	return ele;
+}
+
+function createFolderContextMenu(folderFullname, open, del){
+	return [
+		{
+			label: 'Open',
+			click: () => {open()}
+		},
+		{
+			label: 'Open in terminal',
+			click: () => {
+				spawn(path.resolve(__dirname, '../', 'bin', 'open-terminal.cmd'), [folderFullname]);
+			}
+		},
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Cut',
+			accelerator: 'Ctrl+X',
+			click: () => {console.log('cut')}
+		},
+		{
+			label: 'Copy',
+			accelerator: 'Ctrl+C',
+			click: () => {console.log('copy')}
+		},
+		{
+			label: 'Paste',
+			accelerator: 'Ctrl+V',
+			click: () => {console.log('paste')}
+		},
+		{
+			label: 'Delete',
+			accelerator: 'Delete',
+			click: () => {
+				deleteConfirmationDialog(dialog, file, stat, (res) => {
+					console.log(res);
+				/*	trash([fileName]).then(() => {
+						updateLocation(explorerPath);
+						console.log('done');
+					});*/
+				});
+			}
+		}
+	];
+}
+
+function createFileContextMenu(fileFullname, open, del){
+	return [
+		{
+			label: 'Open',
+			click: () => {open()}
+		},
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Cut',
+			accelerator: 'Ctrl+X',
+			click: () => {console.log('cut')}
+		},
+		{
+			label: 'Copy',
+			accelerator: 'Ctrl+C',
+			click: () => {console.log('copy')}
+		},
+		{
+			label: 'Paste',
+			accelerator: 'Ctrl+V',
+			click: () => {console.log('paste')}
+		},
+		{
+			label: 'Delete',
+			accelerator: 'Delete',
+			click: () => {
+				deleteConfirmationDialog(dialog, file, stat, (res) => {
+					console.log(res);
+					/*	trash([fileName]).then(() => {
+							updateLocation(explorerPath);
+							console.log('done');
+						});*/
+				});
+			}
+		}
+	];
+}
+
+
+/*
 function updateLocation(explorerPath, targetDir, parentFolder){
 	var locationString = explorerPath[explorerPath.length - 1];
 
@@ -70,45 +353,12 @@ function updateLocation(explorerPath, targetDir, parentFolder){
 		appendFiles(explorerPath, files, parentFolder);
 		explorerPath.push(locationString);
 
-		var pathContainer = document.querySelector('.path-container');
-		var splitLocation = explorerPath[explorerPath.length - 1].split(path.sep);
-
-		//remove empty string
-		if (splitLocation[splitLocation.length - 1] === '') {
-			splitLocation.splice(-1, 1);
-		}
-		pathContainer.innerHTML = '';
-
-		splitLocation.forEach(function(item, index){
-			let locEl = document.createElement('span');
-			locEl.setAttribute('class', 'location-element');
-
-			locEl.innerHTML = item;
-
-			//get current iteration location
-			let locationSliced = splitLocation.slice(0, index + 1);
-			let currLocation = locationSliced.join(path.sep);
-
-			locEl.addEventListener('click', function(e){
-				//fix C location to be at root
-				locationSliced.length === 1 ? explorerPath.push('C:\\') : explorerPath.push(currLocation);
-				updateLocation(explorerPath);
-			});
-
-			pathContainer.appendChild(locEl);
-
-			if (index < splitLocation.length - 1){
-				let locSep = document.createElement('span');
-				locSep.setAttribute('class', 'location-separator');
-				locSep.innerHTML = '<i class="icon-right-open"></i>';
-				pathContainer.appendChild(locSep);
-			}
-		});
+		
 	});
-}
+}*/
 
+/*
 function appendFiles(explorerPath, files, parentFolder){
-	let explorerContainer = document.querySelector('#explorer-container');
 	let navigationContainer = document.querySelector('#navigation-container');
 	let subfolderContainer = document.createElement('ul');
 
@@ -217,7 +467,7 @@ function appendFiles(explorerPath, files, parentFolder){
 								/*	trash([fileName]).then(() => {
 										updateLocation(explorerPath);
 										console.log('done');
-									});*/
+									});*//*
 								});
 							}
 						}
@@ -256,7 +506,7 @@ function appendFiles(explorerPath, files, parentFolder){
 									/*	trash([fileName]).then(() => {
 											updateLocation(explorerPath);
 											console.log('done');
-										});*/
+										});*//*
 								});
 							}
 						}
@@ -270,6 +520,7 @@ function appendFiles(explorerPath, files, parentFolder){
 		});
 	}, 0);
 }
+*/
 
 function Selected(className){
 	var elements = [];
