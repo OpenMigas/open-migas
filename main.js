537112d5c5a670fb63940ad48a958a9436ba5444
unknown <juliansotoponce@gmail.com>
2018-11-07 21:09:40 -0800

    Add OpenMigas icon

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 function createWindow(){
-    const win = new BrowserWindow({width: 800, height: 600, frame: false});
+    const win = new BrowserWindow({width: 800, height: 600, frame: false, icon: './app/img/openmigas-icon.png'});
     win.loadFile('./app/index.html');
     win.openDevTools();
     win.on('close', function(){
